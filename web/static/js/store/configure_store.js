6b8e2946ef92c4f5c46abe600edfd40f10a7ad11
wannabefro <sam.mctaggart@gmail.com>
2016-09-17 16:28:56 +0100

    Add redux-thunk to allow async actions

--- web/static/js/store/configure_store.js
+++ web/static/js/store/configure_store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const configureStore = () => createStore(rootReducer);
+const configureStore = () => createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
 
 export default configureStore;
