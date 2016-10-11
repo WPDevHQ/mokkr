32b286f3989368af77dbe533b3a59204373a826c
wannabefro <sam.mctaggart@gmail.com>
2016-10-11 23:02:07 +0100

    Remove unused packages

--- start.sh
+++ start.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
 function cleanup {
-  /etc/init.d/xvfb stop
-  kill $CHROMEDRIVER_PID
-  kill $SELENIUM_PID
   kill $REDIS_PID
-  killall java
   rm *.png
 }
 
 trap cleanup EXIT
-export DISPLAY=:99
-/etc/init.d/xvfb stop
-/etc/init.d/xvfb start
-java -jar /opt/selenium/selenium-server-standalone.jar &
-SELENIUM_PID=$!
 redis-server &
 REDIS_PID=$!
 mix deps.get
