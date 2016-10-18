b03200b29822c7191defc93101efc4a83de1a706
Jusna Begum <jb@juweez.co.uk>
2016-10-18 21:08:14 +0100

    Update description about Mokkr

--- web/static/js/components/about_section.jsx
+++ web/static/js/components/about_section.jsx
@@ -7,13 +7,16 @@ const AboutSection = () => (
       <ol>
         <li>Simply enter your website URL in the input field and click “Generate”</li>
         <li>Click “Download image” to download a PNG of your mockup</li>
-        <li>Use your mockup for absolutely whatever you want!</li>
+        <li>Share a link to show others your mockup by using
+        "http://mokkr.brightrobots.io/?url=http://www.yoursite.com"</li>
       </ol>
 
-      <h2>About Responsive Mokkr</h2>
+      <h2>About Mokkr</h2>
       <p>
-        Created by <a href="https://twitter.com/juweez">@juweez</a> and <a href="https://twitter.com/wannabefro">@wannabefro</a>, this free online tool is a huge
-        time saver for web developers and designers.
+        Created by <a href="https://twitter.com/juweez">@juweez</a> and
+        <a href="https://twitter.com/wannabefro">@wannabefro</a>, this free
+        responsive mockup generator is a huge time saver for web developers
+        and designers.
       </p>
       <p>
         If you often need to put together screenshots of websites at various
@@ -21,6 +24,10 @@ const AboutSection = () => (
         visual examples of responsive web design, you’ll know how time
         consuming this is to do manually. This tool is intended to take
         all that effort away for you!
+      </p>
+      <p>
+        <a href="https://github.com/brightrobots/mokkr">View on github</a>.
+        Spot any issues? Let us know!
       </p>
     </div>
   </section>
