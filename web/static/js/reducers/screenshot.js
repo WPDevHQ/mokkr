0b58078c76eb4410238bba5b1e622022e6ab572c
wannabefro <sam.mctaggart@gmail.com>
2016-10-20 21:22:39 +0100

    Move devices into their own file

--- web/static/js/reducers/screenshot.js
+++ web/static/js/reducers/screenshot.js
@@ -1,48 +1,12 @@
 import { SOCKET_CONNECTED, SET_SCREENSHOT, LOADING_CHANGED, SET_URL, SCREENSHOT_ERROR } from '../actions';
+import devices from '../devices';
 
 const initialState = {
   sessionId: null,
   isLoading: false,
   url: null,
   error: null,
-  screenshots: {
-    iMac: {
-      background: 'images/imac.png',
-      src: '',
-      width: '940',
-      height: '875',
-      top: 0,
-      left: 0,
-      offsetTop: '34',
-      offsetLeft: '39',
-      screenshotWidth: '865',
-      screenshotHeight: '487',
-    },
-    iPad: {
-      background: 'images/ipad-air.png',
-      src: '',
-      width: '430',
-      height: '508',
-      top: '270',
-      left: '704',
-      offsetTop: '43',
-      offsetLeft: '65',
-      screenshotWidth: '300',
-      screenshotHeight: '400',
-    },
-    iPhone: {
-      background: 'images/iphone6.png',
-      src: '',
-      width: '155',
-      height: '352',
-      top: '458',
-      left: '622',
-      offsetTop: '39',
-      offsetLeft: '11',
-      screenshotWidth: '135',
-      screenshotHeight: '240',
-    },
-  },
+  screenshots: devices,
 };
 
 function mockup(state = initialState, action) {
