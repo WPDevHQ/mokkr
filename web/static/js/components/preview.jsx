34518eaa4c2d7a27e3df16110abafe1f93ac2289
wannabefro <sam.mctaggart@gmail.com>
2016-10-20 21:33:07 +0100

    Make preview a react component

--- web/static/js/components/preview.jsx
+++ web/static/js/components/preview.jsx
@@ -3,23 +3,29 @@ import { connect } from 'react-redux';
 import ScreenshotPreview from './screenshot_preview';
 import DownloadMockup from './download_mockup';
 
-const preview = ({ screenshots, url }) => {
-  const downloadable = () => (
-    Object.keys(screenshots).every(s => (
+class preview extends React.Component {
+  get downloadable() {
+    let { screenshots } = this.props;
+
+    return Object.keys(screenshots).every(s => (
       screenshots[s].src.length !== 0
     ))
-  );
+  };
 
-  return (
-    <section className="l-content u-text-center">
-      <div className="m-preview-canvas">
-        <svg className="m-scene" width="1135" height="875" viewBox="0 0 1135 875">
-          { Object.keys(screenshots).map(s => <ScreenshotPreview key={s} {...screenshots[s]} />) }
-        </svg>
-      </div>
-      <DownloadMockup previewClass="m-scene" url={url} downloadable={downloadable()} />
-    </section>
-  );
+  render() {
+    let { screenshots, url } = this.props;
+
+    return (
+      <section className="l-content u-text-center">
+        <div className="m-preview-canvas">
+          <svg className="m-scene" width="1135" height="875" viewBox="0 0 1135 875">
+            { Object.keys(screenshots).map(s => <ScreenshotPreview key={s} {...screenshots[s]} />) }
+          </svg>
+        </div>
+        <DownloadMockup previewClass="m-scene" url={url} downloadable={this.downloadable} />
+      </section>
+    );
+  }
 };
 
 preview.propTypes = {
